10ca750562d4695db694c17f74ac072e6ddbad3e
xiaoyumin <xiaoymin@foxmail.com>
2022-08-20 14:23:20 +0800

    update

--- build.bat
+++
@@ -1,5 +0,0 @@
-cd D:\Users\xiaoymin\code\github\xiaoymin.github.io
-
-D:
-
-bundle exec jekyll build
--- debug.bat
+++
@@ -1,6 +0,0 @@
-cd D:\Users\xiaoymin\code\github\xiaoymin.github.io
-
-D:
-
-
-bundle exec jekyll serve --watch
